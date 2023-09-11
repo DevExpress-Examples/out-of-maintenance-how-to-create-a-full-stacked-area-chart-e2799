2e28de539bfed8e649a6d3526e90cb7ba1b6c4ea
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-11 12:18:45 +0400

    Change example status to out of maintenance

--- CS/CompoundAreaChart/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Controls;
-
-namespace CompoundAreaChart {
-    public partial class MainPage : UserControl {
-        public MainPage() {
-            InitializeComponent();
-        }
-    }
-}
